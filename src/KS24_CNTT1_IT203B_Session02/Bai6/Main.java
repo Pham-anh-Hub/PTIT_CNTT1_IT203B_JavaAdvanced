02277c359c69660ce43dafd57648876e99ff5ab7
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2026-03-09 17:17:40 +0700

    update Ex6 ss2

--- src/KS24_CNTT1_IT203B_Session02/Bai6/Main.java
+++ src/KS24_CNTT1_IT203B_Session02/Bai6/Main.java
@@ -2,11 +2,20 @@ package KS24_CNTT1_IT203B_Session02.Bai6;
 
 import KS24_CNTT1_IT203B_Session02.Bai4.User;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         UserProcessor up  = (u) -> UserUtils.convertToUpperCase(u); // Sử dụng Lamda
 
         UserProcessor userProcessor = UserUtils::convertToUpperCase; // Áp dụng Method Expression
 
+        User newUser = new User("Nguyen Van A", 20, "Nam");
+        Supplier<String> supplier = newUser::toString;
+        System.out.println(supplier.get());
+
+        String result = userProcessor.process(newUser);
+        System.out.println(result);
     }
 }
